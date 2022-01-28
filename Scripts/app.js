e1db5f2361e05625b51ee35a5f38d62afb51fdc0
NithusanK <nithusank@hotmail.com>
2022-01-28 11:40:10 -0500

    updated ICE-3

--- Scripts/app.js
+++ Scripts/app.js
@@ -45,6 +45,10 @@
 
         // ES6 and HTML5 => Template Strings => "Super Strings"
 
+        //Test new Contact Class
+        let darryl = new Contact("Darryl Olsen", "555-555-5555", "darryl.olsen@example.com");
+        console.log(darryl.toString());
+
     }
 
     function DisplayProductsPage()
@@ -65,6 +69,62 @@
     function DisplayContactPage()
     {
         console.log("Contact Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        sendButton.addEventListener("click", function(event)
+        {
+
+            //event.preventDefault(); //for testing only
+
+            if(subscribeCheckbox.checked)
+            {
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = "";
+
+            let keys = Object.keys(localStorage); // returns a list of keys from localStorage
+
+            let index = 1;
+
+            // for every key in the keys string array
+            for(const key of keys)
+            {
+                let contactData = localStorage.getItem(key);    // get localStorage data value
+
+                let contact = new Contact();    // creates an empty contact object
+                contact.deserialize(contactData);
+
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>`;
+
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
     }
 
 
@@ -89,6 +149,9 @@
                 break;
             case "Contact Us":
                 DisplayContactPage();
+                break;
+            case "Contact-List":
+                DisplayContactListPage();
                 break;
         }
 
