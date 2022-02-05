0ae2fff85fbb4ac9002fe4e5703709b796a12844
NithusanK <nithusank@hotmail.com>
2022-02-04 23:38:35 -0500

    updated files

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,36 +5,68 @@
     
     function DisplayHomePage()
     {
-        console.log("Home Page");
-        let AboutUsButton = document.getElementById("AboutUsButton");
-        AboutUsButton.addEventListener("click", function()
+        //old code
+        // console.log("Home Page");
+        // let AboutUsButton = document.getElementById("AboutUsButton");
+        // AboutUsButton.addEventListener("click", () =>
+        // {
+        //     location.href = "about.html";
+        // });
+
+        // 1) Fattest Memory Footprint
+        //jQuery way - get all elements with an id of AboutUsButton and for each element add a "click" event
+        $("#AboutUsButton").on("click", () =>
         {
             location.href = "about.html";
         });
 
+        // 2) Second Fattest - because it returns a collection of elements
+        //Javascript way - get all elements wuth an id of AboutUsButton for each element, loop...
+        // document.querySelectorAll("#AboutUsButton").forEach(element => 
+        // {
+        //     //for each element, add a "click" event
+        //     element.addEventListener("click", () =>
+        //     {
+        //       location.href = "about.html";  
+        //     });
+        // });
+        
+        // 3) Pretty Lean
+        //JavaScript way - get an element that matches an id of AboutUsButton and add a "click" event
+        // document.querySelector("#AboutUsButton").addEventListener("click", () =>
+        // {
+        //     location.href = "about.html";  
+        // });
+
+        
+
+
         // Step 1 - get a reference to an entry point(s) (insertion / deletion)
-        let MainContent = document.getElementsByTagName("main")[0];
-        let DocumentBody = document.body;
+        //let MainContent = document.getElementsByTagName("main")[0];
+        //let DocumentBody = document.body;
 
         // Step 2 - Create an HTML Element in memory
-        let MainParagraph = document.createElement("p");
-        let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+        // let MainParagraph = document.createElement("p");
+        // let Article = document.createElement("article");
+        // let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
 
         // Step 3 - Configure new Element
-        MainParagraph.setAttribute("id", "MainParagraph");
-        MainParagraph.setAttribute("class", "mt-3");
-        let FristString = "This is";
-        let SecondString = `${FristString} the Main Paragraph`;
-        MainParagraph.textContent = SecondString;
-        Article.setAttribute("class", "container");
+        // MainParagraph.setAttribute("id", "MainParagraph");
+        // MainParagraph.setAttribute("class", "mt-3");
+        // let FristString = "This is";
+        // let SecondString = `${FristString} the Main Paragraph`;
+        // MainParagraph.textContent = SecondString;
+        // Article.setAttribute("class", "container");
 
         // Step 4 - perform insertion / deletion
 
         // example of insert after (append)
-        MainContent.appendChild(MainParagraph);
-        Article.innerHTML = ArticleParagraph;
-        DocumentBody.appendChild(Article);
+        //MainContent.appendChild(MainParagraph);
+        //Article.innerHTML = ArticleParagraph;
+        $("main").append(`<p id="MainParagraph" class="mt-3">This is the Main Paragraph</p>`);
+        $("body").append(`<article class="container">
+        <p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>
+        </article>`);
 
         // example of insert before
         //MainContent.before(MainParagraph);
@@ -45,8 +77,8 @@
 
         // ES6 and HTML5 => Template Strings => "Super Strings"
 
-        //Test new Contact Class
-        let darryl = new Contact("Darryl Olsen", "555-555-5555", "darryl.olsen@example.com");
+        //Test new core.Contact Class
+        let darryl = new core.Contact("Darryl Olsen", "555-555-5555", "darryl.olsen@example.com");
         console.log(darryl.toString());
 
     }
@@ -66,6 +98,24 @@
         console.log("About Us Page");
     }
 
+    /**
+     * Adds a Contact Object to localStorage
+     *
+     * @param {string} fullName
+     * @param {string} contactNumber
+     * @param {string} emailAddress
+     */
+    function AddContact(fullName, contactNumber, emailAddress)
+    {
+        let contact = new core.Contact(fullName, contactNumber, emailAddress);
+        if(contact.serialize())
+        {
+            let key = contact.FullName.substring(0, 1) + Date.now();
+
+            localStorage.setItem(key, contact.serialize());
+        }
+    }
+
     function DisplayContactPage()
     {
         console.log("Contact Page");
@@ -80,13 +130,7 @@
 
             if(subscribeCheckbox.checked)
             {
-                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
-                if(contact.serialize())
-                {
-                    let key = contact.FullName.substring(0, 1) + Date.now();
-
-                    localStorage.setItem(key, contact.serialize());
-                }
+                AddContact(fullName.value, contactNumber.value, emailAddress.value);
             }
         });
     }
@@ -108,7 +152,7 @@
             {
                 let contactData = localStorage.getItem(key);    // get localStorage data value
 
-                let contact = new Contact();    // creates an empty contact object
+                let contact = new core.Contact();    // creates an empty contact object
                 contact.deserialize(contactData);
 
                 data += `<tr>
@@ -116,17 +160,106 @@
                 <td>${contact.FullName}</td>
                 <td>${contact.ContactNumber}</td>
                 <td>${contact.EmailAddress}</td>
-                <td></td>
-                <td></td>
-                </tr>`;
+                <td class="text-center"><button value="${key}" class="btn btn-primary btn-sm edit"><i class="fas fa-edit fa-sm"></i> Edit</button></td>
+                <td class="text-center"><button value="${key}" class="btn btn-danger btn-sm delete"><i class="fas fa-trash-alt fa-sm"></i> Delete</button></td>
+                </tr>
+                `;
 
                 index++;
             }
 
             contactList.innerHTML = data;
+
+            $("#addButton").on("click",() =>
+            {
+                location.href = "edit.html#add";
+            });
+
+            $("button.delete").on("click", function()
+            {
+                if(confirm("Are you sure?"))
+                {
+                    localStorage.removeItem($(this).val());
+                }
+                
+                //refresh after deleting
+                location.href = "contact-list.html";
+            });
+
+            $("button.edit").on("click", function()
+            {
+                location.href = "edit.html#" + $(this).val();
+            });
         }
     }
 
+    function DisplayEditPage()
+    {
+        console.log("Edit Page");
+
+        let page = location.hash.substring(1);
+
+        switch(page)
+        {
+            case "add":
+                {
+                    $("main>h1").text("Add Contact");
+
+                    $("#editButton").html(`<i class="fas fa-plus-circle fa-lg"></i> Add`);
+
+                    $("#editButton").on("click", (event) =>
+                    {
+                        event.preventDefault();
+                        // Add Contact
+                        AddContact(fullName.value, contactNumber.value, emailAddress.value);
+                        // refresh the contact-list page
+                        location.href = "contact-list.html"
+                    });
+
+                    $("#cancelButton").on("click", () =>
+                    {
+                        location.href = "contact-list.html"
+                    });
+                }
+                break;
+            default:
+                {
+                    // get the contact info from localStorage
+                    let contact = new core.Contact();
+                    contact.deserialize(localStorage.getItem(page));
+
+                    // display the contact info in the edit form
+                    $("#fullName").val(contact.FullName);
+                    $("#contactNumber").val(contact.ContactNumber);
+                    $("#emailAddress").val(contact.EmailAddress);
+
+                    // when editButton is pressed, update the contact
+                    $("#editButton").on("click", (event) =>
+                    {
+                        event.preventDefault();
+
+                        // get any changes from the form
+                        contact.FullName = $("#fullName").val();
+                        contact.ContactNumber = $("#contactNumber").val();
+                        contact.EmailAddress = $("#emailAddress").val();
+
+
+                        // replace the item in localStorage
+                        localStorage.setItem(page, contact.serialize());
+
+                        // return to the contact-list
+                        location.href = "contact-list.html";
+                        
+                    });
+                    
+                    $("#cancelButton").on("click", () =>
+                    {
+                        location.href = "contact-list.html"
+                    });
+                }
+                break;
+        }
+    }
 
     //named function option
     function Start()
@@ -153,10 +286,11 @@
             case "Contact-List":
                 DisplayContactListPage();
                 break;
+            case "Edit":
+                DisplayEditPage();
+                break;
         }
 
-
-        
     }
 
     window.addEventListener("load", Start);
